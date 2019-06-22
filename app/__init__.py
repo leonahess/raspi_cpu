ac7ab981c0ad90af016a319af27d8db68159bd8e
Leon <32615985+leonhess@users.noreply.github.com>
2019-06-22 16:06:22 +0200

    added retention policy checking

--- app/__init__.py
+++ app/__init__.py
@@ -8,18 +8,40 @@ print("> checking if database 'telegraf' exists ...")
 
 database_list = client.get_list_database()
 smarthome_exists = False
+retention_exits = False
 
 for s in range(0, len(database_list)):
-    if database_list[s]['name'] == config.influx_database:
+    if database_list[s]['name'] == 'telegraf':
         smarthome_exists = True
-
         print("< database 'telegraf' exists")
+
 
 if not smarthome_exists:
 
-    print("< database 'telegraf' does not exist!")
+    print("< database 'smarthome' does not exist!")
     print("> creating database 'telegraf' ...")
 
     client.create_database('telegraf')
 
     print("< created database 'telegraf'!")
+
+retention_list = client.get_list_retention_policies("telegraf")
+database_list = client.get_list_database()
+
+print("> checking retention policy")
+for s in range(0, len(database_list)):
+    if database_list[s]['name'] == 'telegraf':
+        for rp in range(0, len(retention_list)):
+            if retention_list[rp]['name'] == config.influx_retention_policy:
+                print("< correct retention policy exists")
+                retention_exits = True
+
+
+if not retention_exits:
+    print("< correct retention policy does not exists")
+    print("> creating correct retention policy")
+
+    client.create_retention_policy(config.influx_retention_policy, config.influx_retention_policy, 1,
+                                   database="telegraf")
+
+    print("< created correct retention policy")
--- config.py
+++ config.py
@@ -2,3 +2,17 @@ influx_ip = "192.168.66.56"
 influx_database = "telegraf"
 influx_port = "8086"
 hostname = "raspi-cluster-3"
+influx_retention_policy = "2w"
+
+'''
+possible retention intervals:
+
+ns	nanoseconds (1 billionth of a second)
+u or µ	microseconds (1 millionth of a second)
+ms	milliseconds (1 thousandth of a second)
+s	second
+m	minute
+h	hour
+d	day
+w	week
+'''
